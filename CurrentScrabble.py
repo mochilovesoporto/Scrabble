b38babce0ae1ebaa95f9bbd1e9e93c0a9409a1b4
mochilovesoporto <kentconnor45@gmail.com>
2023-03-11 13:38:16 +1100

    Scrabble - Tidy

--- CurrentScrabble.py
+++ CurrentScrabble.py
@@ -1,7 +1,6 @@
 # Scrabble
 import enchant
 
-#Print str to user and introduce game as well as game rules
 print(''' Hello! Welcome to Scrabble. \n
 In Scrabble each letter of the alphabet is assigned a number of points.\n
 Players are asked to submit words, these words are then graded and points are awarded to the player. \n
@@ -25,8 +24,7 @@ players = []
 players_words = [[]]
 player_to_points = {}
 
-#Request number of players to determine how many rounds of the game there are
-
+#Request numPlayers
 while 0 == 0:
   num_players = input("How many players are there?: ")
   if num_players.isdigit():
@@ -35,22 +33,16 @@ while 0 == 0:
   else:
     print("\nYou did not enter a number, please make sure you enter a number not letters.\n")
 
-#For each player that is found in the number of players given to us by the user, ask them to enter their name and add the input to the empty players list, while also creating a new empty list inside players_words to house their words
+#Add players to list
 for player in range(num_players):
   player_input = input("\nPlease enter their names one at a time: ")
   players.append(player_input.title())
   players_words.append([])
 
+#Zip player & word lists
 player_to_words = {key: value for key, value in zip(players, players_words)}
 
 #Functions
-
-
-# Create a for loop that loops through each player
-# Ask each player for a word,
-# Create temporary variable to house invidual players word lists
-# Add new word to that players list
-# Print each players score for their word
 def add_playerWords():
   for i in range(len(players)):
     print("\n" + players[i] + " please enter a scrabble word: ")
@@ -64,21 +56,12 @@ def add_playerWords():
     else:
       print("\nThis is not a real word, please make sure you are entering dictionary words only.\n")
 
-# Takes in word from players_word dict.
-# For each letter in that word it gets the associated points allocation from letters_to_points dict & adds it to a temp. variable points_total
-# Once loop ends all points from that word are returned to the function
 def score_word(word):
   points_total = 0
   for letter in word:
     points_total += letters_to_points.get(letter)
   return(points_total)
 
-# Unzip nest dictionairy
-# For each key:value object (players: words) in the players and words dict.
-# For each word within the words (values) pass the word into the score_word function
-# Store the output of the score_word function (points) in temp var.
-# Exit second for loop and pass temp var (points) into player_to_points dict. as a value under each players key
-# Print all players and their points
 def point_total_for_allPlayers():
   for player, words in player_to_words.items():
     player_points = 0
@@ -88,11 +71,6 @@ def point_total_for_allPlayers():
   print(player_to_points)
 
 # Main game loop
-# Loop both functions so that each player gets to input a word 3 times
-# Create temp var with 3 rounds
-# Create while loop that runs while rounds is greater than zero
-# Each loop -= from temp var
-# Run functions to ask for words, calculate scores, and add scores to each player
 def main():
   while 0 == 0:
     rounds = input("\nPlease enter the number of rounds that you would like to play (Each player will get one turn per round): ")
