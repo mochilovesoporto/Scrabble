47fd8aaba4539c2bc15070aa73cfa1b440ca9eb4
mochilovesoporto <kentconnor45@gmail.com>
2023-03-22 17:22:30 +1100

    Wyrdle - updated rich

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
 import wyrdle
 wyrdle.refresh_page("Wyrdle")
 
+wyrdle.console.print("Look at me!", style="warning")
+wyrdle.main()
--- wyrdle.py
+++ wyrdle.py
@@ -1,53 +1,70 @@
 import random
 from rich.console import Console
 from rich.theme import Theme
+from string import ascii_letters
 
-console = Console(width = 100, theme=Theme({"warning": "red on yellow"}))
-console.rule(":leafy_green: Wyrdle :leafy_green:")
 
 #Key:
 
 # #correct = same letter, same place
-# Misplaced  same letter, wrong place
-# Wrong - wrong
+# Misplaced = same letter, wrong place
+# Wrong = wrong
 
 def main():
     #Pre-process --> Everything needs happen before you main loop runs
+    global console
+    console = Console(width=120, theme=Theme({"warning": "red on yellow"}))
+    refresh_page("Wyrdle")
+
     intro()
     length = word_length()
-    guess_word = get_random_word(length)
+    word = get_random_word(length)
     guesses = ["_" * (int(length))] * 6
 
     #Process (main loop) --> Code executed during main game loop
-    for guess_num in range(1, 7):
-        print(guess_word)
-        guess = input("\nGuess " + str(guess_num) + ": ")
-        show_guess(guess_word, guess)
-        if guess == guess_word:
-            print("\nWoohoo! You got correctly guessed the Worlde word!")
+    for idx in range(6):
+        print(word)
+        refresh_page("Guess {}".format(idx + 1))
+        show_guesses(guesses, word)
+        guesses[idx] = input("\nGuess word: ")
+        if guesses[idx] == word:
             break
 
     # Post-process --> Clean-up post main loop
-    else:
-        game_over(guess_word)
+    game_over(guesses, word, correctly_guessed= guesses[idx] == word)
 
 def intro():
-    print("""\nHi! Welcome to Wordle!
-    Worlde is a game in which you must guess a word.
-    You are given 6 guesses.
-    Each time you guess, if the letters in your guess match the letters and order in the Wordle word they will be printed on the screen as "Correct".
-    If you guess a correct letter but not in the right order, it will be printed on the screen as "Misplaced".
-    If your has letters that are not in the Wordle word at all, then they will be printed on the screen as "Incorrect".
+    console.print("""\nHi! Welcome to Wyrdle!
+    
+    Wyrlde is a game in which you must guess a secret randomly generated word.
+    This word could be a type of animal, reptile, insect or fish.
+    You are given a total of 6 guesses to guess the secret word.
+    Pretty tricky right!?
+    
+    Each guess will be shown on the screen with each letter highlighted in a color:
+        [bold white on green]Green[/] = Correct letters in the correct order.
+        [bold white on yellow]Yellow[/] = Correct letters in the wrong order.
+        [white on #666666]Grey[/] = Wrong letters. 
+    
     Let's begin!""")
 
+
 def word_length():
-    guess_word_length = input("\nFirst, decide what is the maximum length of the Wordle word that you wil have to guess. Please enter a number ONLY for this: ")
-    return guess_word_length
+    wordLength = input("\nFirst, decide what is the maximum length of the Wordle word that you wil have to guess. \nPlease enter a number ONLY for this: ")
+    while wordLength.isdigit() == False:
+        wordLength = console.input("[red on yellow]You have not entered a number. Please try again:[/]")
+    else:
+        return wordLength
+
 
 def get_random_word(length):
     with open('animals.txt') as animals:
-        word_list = [line.lower().strip() for line in animals.readlines() if (len(line) - 1) == int(length) and all(letter != " " for letter in line)]
+        word_list = [
+            line.lower().strip() for line in animals.readlines()
+            if (len(line) - 1) == int(length) and all(letter != " " for letter in line)
+        ]
     return random.choice(word_list)
+
 
 def show_guess(word, guess):
 
@@ -65,13 +82,39 @@ def show_guess(word, guess):
     print("\nCorrect letters: ", ",".join(correct_letters))
     print("Misplaced letters: ", ",".join(misplaced_letters))
     print("Incorrect letters: ", ",".join(incorrect_letters))
+    print("\n5")
+
+
+def show_guesses(guesses, word):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append("[{}]{}[/]".format(style, letter))
+        console.print("".join(styled_guess), justify = "center")
+
 
 def refresh_page(headline):
     console.clear() #clear screen
     console.rule("[bold blue]:leafy_green: {} :leafy_green:[/]\n".format(headline)) # print headline on top of screen -- rule = horizontal rule for decoration
 
-def game_over(guess_word):
-    print("\nYou have lost! Better luck next time. The Wordle word was: " + guess_word)
+
+def game_over(guesses, word, correctly_guessed):
+    refresh_page("Game Over")
+    show_guesses(guesses, word)
+    if correctly_guessed:
+        print("\nYou won! You got correctly guessed the Wyrdle word!")
+    else:
+        print("You lost! The Wyrdle word was: {}".format(word))
+
+
 
 if __name__ == "__main__":
     main()
